e03cb2274a1d2f85dcc4f6d139969094e8850b11
junstyle <junstyle@git>
2019-04-04 17:45:13 +0800

    update

---
+++ tool/install-vnc.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+yum install tigervnc-server -y
+
+RELEASE_RPM=$(rpm -qf /etc/redhat-release)
+RELEASE=$(rpm -q --qf '%{VERSION}' ${RELEASE_RPM})
+
+case ${RELEASE} in
+	6*)
+		install_on_centos6
+		;;
+	7*)
+		install_on_centos7
+		;;
+esac
+
+install_on_centos6(){
+	cat >>/etc/sysconfig/vncservers<<eof
+VNCSERVERS="1:root"
+VNCSERVERARGS[1]="-geometry 800x600"
+eof
+
+	vncpasswd
+
+	chkconfig vncserver --level 345 on
+
+	sed -i 's/twm &/#twm &/g' ~/.vnc/xstartup
+	echo 'gnome-session &' >> ~/.vnc/xstartup
+
+	yum groupinstall "X Window System" -y
+	yum install gnome-classic-session gnome-terminal nautilus-open-terminal control-center liberation-mono-fonts -y
+	yum groupinstall "fonts" -y
+
+	service vncserver start
+}
+
+install_on_centos7(){
+	echo 'nothing'
+}
