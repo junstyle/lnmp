771f43576f0832a0eb8328b621fe90c4e516e79a
junstyle <junstyle@git>
2019-04-08 17:22:38 +0800

    update

--- lnmp.sh
+++ lnmp.sh
@@ -39,8 +39,8 @@ install_php(){
 	mkdir -v /home/etc
 	mkdir -v /home/log
 
-	case ${RELEASE} in
-		6*)
+	# case ${RELEASE} in
+	# 	6*)
 			# yum install http://rpms.remirepo.net/enterprise/remi-release-6.rpm -y
 			# yum -y install php73-php-fpm php73-php-cli php73-php-xml php73-php-gd php73-php-mysqlnd php73-php-pdo php73-php-pecl-mcrypt php73-php-mbstring php73-php-json php73-php-pgsql php73-php-opcache php73-php-pecl-redis4 php73-php-devel php73-php-phalcon3
 
@@ -58,7 +58,7 @@ install_php(){
 			# sed -i 's@user = apache@user = www@' /home/etc/php-fpm.d/www.conf
 			# sed -i 's@group = apache@group = www@' /home/etc/php-fpm.d/www.conf
 			# ;;
-		7*)
+		# 7*)
 			# yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 			yum -y install php72u-fpm php72u-cli php72u-xml php72u-gd php72u-mysqlnd php72u-pdo php72u-mcrypt php72u-mbstring php72u-json php72u-pgsql php72u-opcache php72u-pecl-redis php72u-devel
 
@@ -76,7 +76,7 @@ install_php(){
 			sed -i 's@user = php-fpm@user = www@' /etc/php-fpm.d/www.conf
 			sed -i 's@group = php-fpm@group = www@' /etc/php-fpm.d/www.conf
 			;;
-	esac
+	# esac
 
 	start_service php-fpm
 }
